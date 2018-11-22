a8d50884c2c06ec2e8bd4ad2269c56dc2f28a07e
asdfmovie99e <aaa59@hotmail.de>
2018-11-22 12:11:44 +0100

    :)

--- src/helper/MathHelper.java
+++ src/helper/MathHelper.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 public class MathHelper {
 
     private static HashMap<Double, Double> sigmoidMap;
+    private static boolean sigmoidMapInitialized = false;
 
     public static void start() {
         // wird ausgeführt um die HashMaps zu erstellen, welche die Berechnung verschnellern
+        sigmoidMapInitialized = true;
         sigmoidMap = new HashMap<Double, Double>();
         for (int i = -600; i <= 600; i += 1) {
             double iDouble = i;
@@ -17,6 +19,7 @@ public class MathHelper {
     }
 
     public static Double getSigmoidApprox(Double sigmoidX){
+        if (!sigmoidMapInitialized) MathHelper.start();
         if(sigmoidX > 5.9d) return 1d;
         if(sigmoidX < -5.9d) return 0d;
         int MapIndex = (int) (sigmoidX * 100);
