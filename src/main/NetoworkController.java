924ff95a2634b671cc39425ce79ee3658b2f50e1
JensK <aaa59@hotmail.de>
2018-11-22 20:39:22 +0100

    ..

--- src/main/NetoworkController.java
+++ src/main/NetoworkController.java
@@ -5,5 +5,7 @@ public class NetoworkController {
     private static final int ANZAHL_HIDDEN_NEURONEN_ONE = 748;
     private static final int ANZAHL_HIDDEN_NEURONEN_TWO = 748;
     private static final int ANZAHL_OUTPUT_NEURONEN = 748;
+    private static final int ANZAHL_BILDER = 59000;
+
 
 }
--- src/main/PictureCoder.java
+++ src/main/PictureCoder.java
@@ -1,4 +1,10 @@
 package main;
 
 public class PictureCoder {
+
+    public Double[][] get2DPictureArray(int pictureId) {
+        //gibt ein 2 dimensionales Array aus in dem die Pixel des Bildes pictureId gespeichert sind. jeder wert ist zwischen 0(weiss) und 1(schwarz)
+        return null;
+    }
+
 }
