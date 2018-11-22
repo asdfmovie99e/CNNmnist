5b55c587723bdaab3b551528ddae5a64b25009e7
JensK <aaa59@hotmail.de>
2018-11-22 20:28:30 +0100

    jonas' und meine klasse erstellt

---
+++ src/main/NetoworkController.java
@@ -0,0 +1,9 @@
+package main;
+
+public class NetoworkController {
+    private static final int ANZAHL_INPUTN_EURONEN = 748;
+    private static final int ANZAHL_HIDDEN_NEURONEN_ONE = 748;
+    private static final int ANZAHL_HIDDEN_NEURONEN_TWO = 748;
+    private static final int ANZAHL_OUTPUT_NEURONEN = 748;
+
+}
