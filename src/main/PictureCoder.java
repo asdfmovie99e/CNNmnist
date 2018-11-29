46cfa1857d048230a759c2ec9a5e4caefed6326f
JeyLid <38353342+JeyLid@users.noreply.github.com>
2018-11-29 14:14:17 +0100

    Bild als String gespeichert? Array als Byte

--- src/main/PictureCoder.java
+++ src/main/PictureCoder.java
@@ -4,7 +4,7 @@ public class PictureCoder
 {
     public String picture;
     public byte [][] pixel;
-
+    
     public PictureCoder(String picture, byte [][] pixel)
     {
         this.picture = picture;
