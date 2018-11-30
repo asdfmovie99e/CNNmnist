70e6644659b901788317276cbdd1f1a9c018a7fc
asdfmovie99e <aaa59@hotmail.de>
2018-11-30 12:07:10 +0100

    inputneuronen können jetzt die rohen input bytes empfangen

--- src/main/InputNeuron.java
+++ src/main/InputNeuron.java
@@ -10,6 +10,18 @@ public class InputNeuron extends Neuron {
     this.id=id;
     }
 
+    public void receiveRawByte(byte b){
+        double result;
+        if (b < 0 ) {
+            result =((double) ((128 + b)  + 128))/255;
+        } else {
+            result = ((double) b)/255;
+        }
+        setInputSum(result);
+    }
 
+    private void setInputSum(Double inputSum){
+        this.inputSum = inputSum;
+    }
 
 }
