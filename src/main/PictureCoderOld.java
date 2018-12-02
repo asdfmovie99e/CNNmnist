6f65be25c66354465fa376f559b8fff1d2c202ff
asdfmovie99e <aaa59@hotmail.de>
2018-12-02 22:25:54 +0100

    ...

---
+++ src/main/PictureCoderOld.java
@@ -0,0 +1,14 @@
+package main;
+
+public class PictureCoderOld {
+
+    public static Byte[][] get2DPictureArray(int pictureId) {
+        //gibt ein 2 dimensionales Array aus in dem die Pixel des Bildes pictureId gespeichert sind. jeder wert ist zwischen 0(weiss) und 1(schwarz)
+        // das erste [] gibt die x koordinate an und das zweite [] gibt die y koordinate an
+        return null;
+    }
+
+    public static Integer getLabel(int pictureId){
+        return  null;
+    }
+}
