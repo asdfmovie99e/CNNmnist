14a4ed879a05f2b6fc6dcad589627d326bf446c0
JeyLid <38353342+JeyLid@users.noreply.github.com>
2018-12-02 19:30:56 +0100

    Array = ArrayList, Bilder einlesen, Pixel/ Bilder auslesen

--- src/main/PictureCoder.java
+++ src/main/PictureCoder.java
@@ -1,56 +1,63 @@
 package main;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class PictureCoder
-{
-    public static Integer [] picture; //Zahlen von 0-9
-    public static byte [][][] pixel; // Bild(0-60k), X, Y
 
-    public void readTrainData ()
+public class PictureCoder {
+    File trainImages = new File("MNIST/train-images.idx3-ubyte");
+    File trainLabels = new File("MNIST/train-images.idx3-ubyte");
+    BufferedImage images;
+    BufferedImage labels;
+    ArrayList<ArrayList<ArrayList<Byte>>> picturelist = new ArrayList<ArrayList<ArrayList<Byte>>>();
+
+
+    public void readTrainData(Image image) //Bilder einlesen
     {
-       File trainImages = new File("MNIST/train-images.idx3-ubyte");
-       File trainLabnels = new File("MNIST/train-images.idx3-ubyte");
-    }
-
-    public byte[][] pixelsOfPicture (Integer a) //0-60k um PixelArray zurückzubekommen
-    {
-        return pixel[a];
-    }
-
-    public Integer[] pictureID () // Nummer, die auf dem Bild zu sehen ist
-    {
-        return picture;
-    }
-
-
-    for (int i = 16; i < 60000*28*28;i++) //60k Bilder á 28*28 Pixel
-    {
-        int modOfPicture = i % (28*28);   // z.B. 1600 mod 784 = 32
-        int roundedPicture = i - modOfPicture;    // 1600 - 32 = 1568
-
-        if (modOfPicture == 0)
+        try
         {
-            int pictureNumber = (roundedPicture / (28*28)); // 1568 / 784 = 2
+            images = ImageIO.read(trainImages);
+            labels = ImageIO.read(trainLabels);
         }
-        else
+        catch (IOException io)
         {
-            int pictureNumber = (roundedPicture / (28*28))+1; // 1568 / 784 = 2 +1, weil 1600 ist 3tes Bild und nicht 2tes
         }
 
-
-        int modOfPixel = i % 28;                // z.B. 30 mod 28 = 2
-        int roundedPixel = i - modOfPixel;      // 30 - 2 = 28
-
-        if (modOfPixel == 0)
+        for(int i=16; i<60000; i++)
         {
-            int rownumber = (roundedPixel / 28);    // 28 / 1 = 1
+            ArrayList<ArrayList<Byte>> picture = new ArrayList<ArrayList<Byte>>(); //bild
+            for(int ii=0; ii<28; ii++)
+            {
+                ArrayList<Byte> row = new ArrayList<Byte>(); // reihe
+                for(int iii=0; iii<28; iii++)
+                {
+                    row.add((byte)(images.getRGB(ii, iii)));
+                }
+                picture.add(row);
+            }
+            picturelist.add(picture);
         }
-        else
-        {
-            int rownumber = (roundedPixel / 28)+1;    // 28 / 1 = 1 +1, weil 30 ist 2te Reihe
-        }
+
     }
-    
+
+    public Byte readPixel(int i, int ii, int iii) //Pixel auslesen
+    {
+        ArrayList<ArrayList<Byte>> twoDPicture = picturelist.get(i); //i = bild
+
+        ArrayList<Byte> pixelRow = twoDPicture.get(ii); // ii = Reihe
+
+        Byte bytewert = pixelRow.get(iii);    // iii = spalte
+        return bytewert;
+    }
+
+    public ArrayList<ArrayList<Byte>> readPicture(int i)  //Bild auslesen
+    {
+        ArrayList<ArrayList<Byte>> twoDPicture = picturelist.get(i); // i = bild
+        return twoDPicture;
+    }
 
 }
