1c60d49e7d8cb762929b9d63abcac81b9513c426
niklasbru <niklas.bruns@gmx.de>
2018-12-07 23:14:03 +0100

    Add Controller

---
+++ src/gui/LoadController.java
@@ -0,0 +1,29 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoadController {
+
+    @FXML
+    private TextField Nr;
+
+    @FXML
+    private Button clickload;
+
+    @FXML
+    private Button clickdelete;
+
+    @FXML
+    void clickdelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickload(ActionEvent event) {
+
+    }
+
+}
---
+++ src/gui/PasswortController.java
@@ -0,0 +1,38 @@
+package gui;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class PasswortController {
+
+    @FXML
+    private CheckBox safelogin;
+
+    @FXML
+    private Button clicklogin;
+
+    @FXML
+    private TextField User;
+
+    @FXML
+    private PasswordField Passwort;
+
+    @FXML
+    private TextField URL;
+
+    @FXML
+    void clicklogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void safelogin(ActionEvent event) {
+
+    }
+
+}
